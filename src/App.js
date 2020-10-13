44582841d95b3e03ddeb1f77cec73dd331e0c154
Patrick Shushereba <pshushereba@gmail.com>
2020-10-12 20:35:20 -0700

    Set up basic theme and installed axios.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./components/styles/Theme.js";
+import Header from "./components/Header.js";
 
 function App() {
-  return <ThemeProvider theme={theme}>Testing!</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Header />
+      Testing!
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/styles/Theme.js
+++ src/components/styles/Theme.js
@@ -4,6 +4,11 @@ const theme = createMuiTheme({
   palette: {
     common: {
       darkBlue: "hsl(209, 23%, 22%)",
+      veryDarkBlue: "hsl(207, 26%, 17%)",
+      darkBlueText: "hsl(200, 15%, 8%)",
+      darkGray: "hsl(0, 0%, 52%)",
+      veryLightGray: "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
     },
   },
 });
